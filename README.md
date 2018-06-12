7673c9c111f84aacd0137608eef9dfa0f558513f
“Vilde <vilde@vevatne.no>
2018-06-11 23:59:01 -0700

    Refactor code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 # 1. The user 'deleted' property has an integer value. I noticed that with all but one of the user instances have that set to the value of 0. With your daffy duck example I made the assumption that deleted values that are anything else than 0 means that the user is deleted.
 
-# 2. That manual testing will suffice. Do to the short timeframe of the challenge I haven't had the chance to write tests for the program. I am aware that this is a vital part of writing code in a production environment and I would have done it if I had more time.
+# 2. That manual testing will suffice. Due to the short timeframe of the challenge I haven't had the chance to write tests for the program. I am aware that this is a vital part of writing code in a production environment and I would have done it if I had more time.
 
 # To run the program:
 
@@ -12,8 +12,6 @@
 
 # - Make sure the db is correctly set up following the instructions in 'instructions.md'
 
-# - Inside the directory run the command (quotes around the input is optional):
+# - Inside the directory run the command (quotes around the input are optional):
 
 	> ruby find_my_teams.rb "daffy_duck@qa.com"
-
-# Full disclosure: When I ran the file yesterday everything worked perfectly, however after the latest mac update I get the error 'Killed: 9' whenever I attempt to run the file, specifically when I run the second query. I'm currently trying to resolve the issue, but I want to submit my submission before it's too late. Hopefully it will work on your computers. 
--- find_my_teams.rb
+++ find_my_teams.rb
@@ -1,16 +1,7 @@
 require 'sqlite3'
 
-# The user input:
-user_input_email = ARGV[0]
-user_email_domain = user_input_email.split('@')[-1]
-
-# Variables for the print statements, defined here so we have access not just in the function scope
-teams_member_in = []
-possible_teams = []
-teams_with_members = []
-
-# Function for formatting an sql 'in' condition
 def in_query_parametization(ids)
+  # Function for formatting an sql 'in' condition
   query_str = '('
   if ids.is_a? Hash 
     arr = ids.map { |k, v| k } 
@@ -18,34 +9,62 @@ def in_query_parametization(ids)
     arr = ids.map { |obj| obj[0] } 
   end 
   query_str.concat(arr.join(', ') + ')')
-end 
+end
 
-# Open up the db file
-SQLite3::Database.open("appeng_take_home_db") do |db|
-	# Find all the teams that the given email is a member of, excluding the deleted
+def find_current_teams(db, user_input_email, teams_member_in)
+  # Find all the teams that the given email is a member of, excluding the deleted
   db.execute("SELECT teams.id, teams.name FROM teams JOIN users ON teams.id=users.team_id WHERE email='#{user_input_email}' AND deleted=0") do |team_obj|
-  	teams_member_in << team_obj
-  end 
-  teams_member_in_str = in_query_parametization(teams_member_in)
+    teams_member_in << team_obj
+  end
+  teams_member_in
+end
+
+def find_possible_teams(db, user_email_domain, teams_member_in_str, possible_teams)
   # Find all the teams that the user is not currently a member of and have the correct email domain
   db.execute("SELECT id, name FROM teams WHERE email_domain='#{user_email_domain}' AND id NOT IN #{teams_member_in_str}") do |team_obj|
     possible_teams[team_obj[0]] = team_obj
   end
-  current_teams_str = in_query_parametization(possible_teams)
+  possible_teams
+end
+
+def teams_with_members(db, possible_teams_str, teams_with_members)
   # Find how many members the possible teams have, sort them in descending order
-  db.execute("SELECT team_id, COUNT(*) AS NUM_OF_USERS_IN_GROUP FROM users WHERE team_id IN #{current_teams_str} GROUP BY team_id ORDER BY NUM_OF_USERS_IN_GROUP DESC") do |team_obj|
-  	teams_with_members << team_obj
+  db.execute("SELECT team_id, COUNT(*) AS NUM_OF_USERS_IN_GROUP FROM users WHERE team_id IN #{possible_teams_str} GROUP BY team_id ORDER BY NUM_OF_USERS_IN_GROUP DESC") do |team_obj|
+    teams_with_members << team_obj
   end
+  teams_with_members
 end
 
-# Print the results to the user:
-print "You are a member of: \n"
-teams_member_in.each do |team_obj|
-	print "#{team_obj[1]} (#{team_obj[0]}) \n"
-end
+def print_team_info(teams_member_in, teams_with_members, possible_teams)
+  # Print the results to the user
+  print "✨ You are a member of: \n"
+  teams_member_in.each do |team_obj|
+	 print "#{team_obj[1]} (#{team_obj[0]}) \n\n"
+  end
 
-print "You are eligible to join: \n"
-teams_with_members.each do |team_obj|
-	team_info = possible_teams[team_obj[0]]
-	print "#{team_info[1]} (#{team_info[0]})         #{team_obj[1]} members\n"
-end
+  print "✨ You are eligible to join: \n"
+  teams_with_members.each do |team_obj|
+	 team_info = possible_teams[team_obj[0]]
+	 print "#{team_info[1]} (#{team_info[0]})         #{team_obj[1]} members\n"
+  end
+end 
+
+def run_file()
+  teams_member_in = []
+  possible_teams = {}
+  teams_with_members = []
+  user_input_email = user_email_domain = ARGV[0].to_s
+  user_email_domain = user_input_email.split('@')[1]
+  # If user provides an invalid email, return and don't run queries
+  return print "You need to provide a valid email" if user_email_domain == nil
+  db = SQLite3::Database.open("appeng_take_home_db")
+  current_teams = find_current_teams(db, user_input_email, teams_member_in)
+  teams_member_in_str = in_query_parametization(teams_member_in)
+  possible_teams = find_possible_teams(db, user_email_domain, teams_member_in_str, possible_teams)
+  possible_teams_str = in_query_parametization(possible_teams)
+  teams_with_members = teams_with_members(db, possible_teams_str, teams_with_members)
+  db.close
+  print_team_info(teams_member_in, teams_with_members, possible_teams)
+end 
+
+run_file
